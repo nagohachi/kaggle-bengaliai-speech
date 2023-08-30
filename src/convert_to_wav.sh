08c7916caa8f899903154fa2ed17285530ed3e03
yukihira-pot <r.magoshi.ma54@gmail.com>
2023-08-30 12:43:56 +0900

    .mp3 -> .wav 変換を追加

---
+++ src/convert_to_wav.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# ディレクトリのパスを変数に設定
+SRC_DIR="../input/bengaliai-speech/train_mp3s"
+DST_DIR="../input/bengaliai-speech/train_wavs"
+
+# .wav 用のディレクトリが存在しない場合、作成
+mkdir -p "$DST_DIR"
+
+# .mp3 ファイルの数をカウント
+TOTAL_FILES=$(find "$SRC_DIR" -type f -name "*.mp3" | wc -l)
+echo "Total .mp3 files: $TOTAL_FILES"
+
+# .mp3 ファイルを .wav ファイルに変換
+COUNTER=0
+for mp3file in "$SRC_DIR"/*.mp3; do
+    # ファイル名のみを取得
+    filename=$(basename -- "$mp3file")
+    # .mp3 拡張子を削除
+    filename_noext="${filename%.*}"
+    # ffmpeg を使って .mp3 から .wav に変換（出力を非表示にする）
+    ffmpeg -i "$mp3file" "$DST_DIR/$filename_noext.wav" > /dev/null 2>&1
+    COUNTER=$((COUNTER+1))
+    echo "Converted $COUNTER/$TOTAL_FILES"
+done
